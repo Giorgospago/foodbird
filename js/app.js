725702b2e88d70582166aad82bf5780c89861dbf
christoforos <papagrigoriou_c@kalia.gr>
2019-03-28 21:56:59 +0200

    update

--- js/app.js
+++ js/app.js
@@ -11,13 +11,27 @@ firebase.initializeApp(config);
 
 
 
-var app = angular.module("foodbird", ["firebase"]);
+var app = angular.module("foodbird", ["firebase", "ngRoute", "ngSanitize", "oitozero.ngSweetAlert"]);
 
-app.controller("MainController", function($scope, $http, $firebaseAuth) {
+app.controller("MainController", function($scope, $http, $firebaseAuth, SweetAlert) {
     var auth = $firebaseAuth();
 
     $scope.user = null;
+    $scope.form = {
+        name: "",
+        email: "",
+        password: "",
+        gender: "male"
+    };
 
+    $scope.resetForm = function() {
+        $scope.form = {
+            name: "",
+            email: "",
+            password: "",
+            gender: "male"
+        };
+    };
     
 
 
@@ -33,12 +47,47 @@ app.controller("MainController", function($scope, $http, $firebaseAuth) {
                 provider: fu.additionalUserInfo.providerId
             };
             
-            $http.post("api/register.php", postData)
-            .then(function(response) {
-                if (response.data.success === true) {
-                    console.log("User Created Successfully !!!");
-                }
+            $scope.registerApi(postData);
+        });
+    };
+
+    $scope.emailPasswordLogin = function() {
+        auth.$signInWithEmailAndPassword($scope.form.email, $scope.form.password)
+        .then(function(firebaseUser) {
+            $scope.resetForm();
+        })
+        .catch(function(error) {
+            SweetAlert.swal({
+                title: "Δεν υπάρχει χρήστης με αυτό το email",
+                text: "Θέλετε να κάνετε εγγραφή ?",
+                type: "warning",
+                showCancelButton: true,
+                cancelButtonText: "Όχι, ευχαριστώ !",
+                confirmButtonColor: "#2ecc71",
+                confirmButtonText: "Ναι θέλω !!!",
+                closeOnConfirm: true
+            }, function() {
+                $("#signupModal").modal('show');
             });
+        });
+    };
+
+    $scope.createUser = function() {
+        auth.$createUserWithEmailAndPassword($scope.form.email, $scope.form.password)
+        .then(function(firebaseUser) {
+            $scope.registerApi({
+                id: firebaseUser.user.uid,
+                name: $scope.form.name,
+                gender: $scope.form.gender,
+                provider: "emailPassword",
+                photo: "",
+                email: $scope.form.email
+            });
+
+            $scope.resetForm();
+        })
+        .catch(function(error) {
+            console.error(error);
         });
     };
 
@@ -60,6 +109,16 @@ app.controller("MainController", function($scope, $http, $firebaseAuth) {
 
     $scope.logOut = function() {
         auth.$signOut();
+    };
+
+    $scope.registerApi = function(postData) {
+        $http.post("api/register.php", postData)
+            .then(function(response) {
+                if (response.data.success === true) {
+                    SweetAlert.swal("Ο χρήστης δημιουργήθηκε με επιτυχία!", "", "success");
+                    $(".modal").modal('hide');
+                }
+            });
     };
 
 });
