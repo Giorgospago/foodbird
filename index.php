55cf14028f08b96ef426233c448a78b6f54b8ed8
christoforos <papagrigoriou_c@kalia.gr>
2019-05-16 20:47:54 +0300

    My awesome update

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 		<!-- Required meta tags -->
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<base href="/birdfood/"/>
+		<base href="<?=$_SERVER['REQUEST_URI']?>"/>
 
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
